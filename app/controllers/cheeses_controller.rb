fb34ff640d829edfdc71d81d7f59ecf839dbc317
Esther Maina <mainaesther77@gmial.com>
2022-11-26 12:31:06 +0300

    Completed code along

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,9 +1,14 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  # end
+  
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, except: [:created_at, :updated_at]
   end
 
   # GET /cheeses/:id
